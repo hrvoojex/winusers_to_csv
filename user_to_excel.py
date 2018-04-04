55662b94db1ff2f82ad9d2853e7326f316947199
Hrvoje <hrvooje@gmail.com>
2018-04-04 07:58:11 +0200

    is_admin() function with ctypes works on local PC, but not on \TSCLIENT mounted drive from krdc remote Kubuntu application

--- user_to_excel.py
+++ user_to_excel.py
@@ -13,21 +13,24 @@ import sys
 import subprocess
 
 
-# def is_admin():
-#     try:
-#         return ctypes.windll.shell32.IsUserAnAdmin()
-#     except:
-#         return False
-#
-# if is_admin():
-#     input("admin sam. prije naredbe if petlja")
-#     subprocess.call(['wmic', '/output:usracc.csv', 'useraccount', 'list', 'full', '/format:csv'])
-#     input("admin sam. poslije naredbe if petlja")
-#
-# else:
-#     input("nisam admin. prije naredbe else petlja")
-#     # Re-run the program with admin rights
-#     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys.argv[0], None, 1)
-#     input("nisam admin. poslije naredbe else petlja")
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
-subprocess.call(['wmic', '/output:usracc.csv', 'useraccount', 'list', 'full', '/format:csv'])
+if is_admin():
+    input("I am admin. Before if loop.")
+    sp = subprocess.Popen(['wmic', '/output:usracc.csv', 'useraccount', 'list', 'full', '/format:csv'],
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = sp.communicate()
+    print(out, err, sp.returncode)
+    input("I am admin. After if loop.")
+
+else:
+    input("I'm not admin. Before else loop.")
+    # Re-run the program with admin rights
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, sys.argv[0], None, 1)
+    input("I'm not admin. After else loop.")
+
+#subprocess.call(['wmic', '/output:usracc.csv', 'useraccount', 'list', 'full', '/format:csv'])
